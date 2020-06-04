01ebf09faf58d4bc0e24b1bbad5470c997fd5deb
franklyT <48483484+franklyT@users.noreply.github.com>
2020-06-04 02:47:38 -0400

    Update init.ts

--- ts/init.ts
+++ ts/init.ts
@@ -1,5 +1,3 @@
-// This is two folders down as it is the only way I can make gulp process it properly at the end of the compiled JS file
-
 const EVENT_WRITE_STYLES = new CustomEvent("writeStyles");
 
 for (const [KEY] of Object.entries(STYLES)) {
