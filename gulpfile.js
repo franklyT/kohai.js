acaddf1511858312e31da6f99ba5392f1a14dcd9
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-04 19:43:40 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -30,13 +30,13 @@ GULP.task("build-ts", function () {
   .pipe(CONCAT('root.min.js'))
   .pipe(GAP.appendFile('./src/js/_root/root.js'))
   .pipe(GBABEL({
-    presets: ['@babel/env', ['minify', {
-      builtIns: false,
+    presets: 
+    ['@babel/preset-env', ['minify', {
       evaluate: false,
       mangle: false,
-    }],],
-    plugins: ["module:faster.js"]
-  }))
+    }],
+  ],
+    plugins: ["module:faster.js"]  }))
   .pipe(GULP.dest("./dist/js"));
 });
 
